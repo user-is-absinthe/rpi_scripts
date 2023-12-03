1d1cb4073431893199b5557a1eef340e259f9b45
user-is-absinthe <38786793+user-is-absinthe@users.noreply.github.com>
2023-12-03 03:28:01 +0300

    Add verbose and DNS

--- remote_server/wireguard_setup/scripts/restore_users.sh
+++ remote_server/wireguard_setup/scripts/restore_users.sh
@@ -4,28 +4,42 @@ KEYS_DIR="keys"
 PUBKEY_SERVER="публичный ключ сервера"
 IP_SERVER="123.456.789.123"
 PORT_SERVER="порт"
+DNS_SERVER="9.9.9.9"
 
 all_config_dir="config_and_qr"
+verbose=false
+
 
 if ! [[ $# -gt 0 ]]
     then
         echo "Error: Missing arguments."
         echo "./restore_users.sh -i {последний октет IP} -u {username without spaces} -f {название интерфейса wg}"
+        echo "Для вывода QR сразу добавить аргумент -v."
         echo "Example:"
         echo "./restore_users.sh -i 97 -u iPhone_Igor -f wg0"
+        echo "          OR"
+        echo "./restore_users.sh -i 97 -u iPhone_Igor -f wg0 -v"
         exit 0
 fi
 
-while getopts i:u:f: flag
+while getopts i:u:f:v flag
 do
     case "${flag}" in
         i) ip=${OPTARG};;
         u) username=${OPTARG};;
         f) wg_interface_name=${OPTARG};;
+        v) verbose=true;;
         *) echo "Error with arguments, run without it."
           exit 1
     esac
 done
+
+# проверка того, что все аргументы переданы (существуют/присвоены)
+if [ -z "$ip" ] || [ -z "$username" ] || [ -z "$wg_interface_name" ]; 
+    then
+        echo 'Missing one or more arguments.' >&2
+        exit 1
+fi
 
 # проверка того, что указанный интерфейс поднят и работает
 if wg | grep -q "$wg_interface_name"
@@ -103,7 +117,7 @@ echo "$public_key" > "${path_to_work_dir}/public.key"
 config="[Interface]
 PrivateKey = ${private_key}
 Address = 10.8.0.${small_ip}/24
-DNS = 9.9.9.9
+DNS = ${DNS_SERVER}
 
 [Peer]
 PublicKey = ${PUBKEY_SERVER}
@@ -120,6 +134,14 @@ echo "$config" > "${all_config_dir}/$dir_name/${ip}_${username}.conf"
 
 # echo "Already done!"
 echo "${username} with IP 10.8.0.${small_ip}"
+
+if [ "$verbose" = true ];
+    then
+        #echo "real verbose"
+        echo "$config" | qrencode -t ansiutf8
+#    else
+#        echo "not verbose"
+fi
 #echo "$config" | qrencode -t ansiutf8
 # cat "${path_to_work_dir}/config.conf"
 
